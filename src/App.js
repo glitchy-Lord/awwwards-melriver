623e1a070f4d23db52fc2f103ff80af40e1695fd
Bhushan Alhat <alhatbhushan@gmail.com>
2021-08-17 17:49:50 +0530

    basic header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Header from './components/Header';
+import './styles/App.scss';
+
 function App() {
-	return <div className='App'>Melriver</div>;
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
